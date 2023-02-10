77faabb1d3985babef3cbc2eb6a57014ea26ce93
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-10 22:00:45 +0600

    Different ways to use addEventLisitener on a button

--- events.js
+++ events.js
@@ -1,3 +1,9 @@
+// Option-1: . 
+// <button onclick="document.body.style.backgroundColor='yellow'">make yellow</button>
+
+
+
+// Option-2 add onclick function on the html element
 function makeGreen(){
     document.body.style.backgroundColor = 'green';
 } 
@@ -15,3 +21,23 @@ const purpleBtn = document.getElementById('purple');
 purpleBtn.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple';
 }
+
+// Option-4
+const pinkBtn = document.getElementById('pink');
+pinkBtn.addEventListener('click', makePink);
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// Option-4 another
+
+const redBtn = document.getElementById('red');
+redBtn.addEventListener('click', function makeRed(){
+    document.body.style.backgroundColor = 'red';
+});
+
+// Importent we will use this sometime
+document.getElementById('goldenRod',).addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenRod';
+})
